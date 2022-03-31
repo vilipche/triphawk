24e74d81a1b2b9d208093c62df3a659449853ae6
vilipche <filipsotiroski@gmail.com>
2022-03-31 23:13:23 +0000

    cron scheduler

--- scripts/scheduler.py
+++ scripts/scheduler.py
@@ -1,7 +1,16 @@
 from crontab import CronTab
-cron = CronTab(user=True)
-job  = cron.new(command='echo hello_world')
+from datetime import date
 
-job.minute.every(1)
+cron = CronTab(user=True)
+
+python3 = '/usr/bin/python3'
+attractions_path = '/home/bdm/triphawk/scripts/collectors/attractions/foursquare.py'
+businesses_path = '/home/bdm/triphawk/scripts/collectors/businesses/yelp.py'
+
+#TODO fix the period
+attractions = cron.new(command=f'{python3} {attractions_path}')
+businesses = cron.new(command=f'{python3} {businesses_path}')
+# attractions.minute.every(1)
+
 
 cron.write()
