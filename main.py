8a5f2fa79003208adf908b2d05fcbc8a2b5a294d
vilipche <filipsotiroski@gmail.com>
2022-03-31 21:42:09 +0000

    one

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 from credentials import keys
 import requests
 
-
+import os
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(keys.api['amadeus']['grant_type'])  # Press Ctrl+F8 to toggle the breakpoint.
-
+    pass
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    api_url = 'https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=CDG&destinationLocationCode=BCN&departureDate=2021-11-01&adults=1&nonStop=false&max=250'
-    response = requests.get(api_url)
-    print(response.json())
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    os.mkdir('test')
